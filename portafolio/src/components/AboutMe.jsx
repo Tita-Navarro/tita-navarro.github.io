fc269c1ca3916f9485088305545f8ae63b5368a8
Martha Navarro <tita1089@gmail.com>
2019-06-21 21:15:12 -0500

    adding media queries into AboutMe component

--- portafolio/src/components/AboutMe.jsx
+++ portafolio/src/components/AboutMe.jsx
@@ -9,14 +9,14 @@ const AboutMe =()=>{
             <div className='About-container div-bg'>
                 <div className='About-brand'>
                     <br/> <br/> <br/>
-                    <img src={require('../images/Castelnav.png')} alt='brand' className='mx-auto d-block img-fluid'/><br/>
+                    <img src={require('../images/Castelnav.png')} alt='brand' className='mx-auto d-block img-fluid resp_img'/><br/>
                     <h2 className='About-info title'>- FrontEnd UX Marketer -</h2>
                 </div>
                 <br/> <br/>
                 <div className="container AboutMe">
                     <div className="row">
                         <div className="col-md">
-                            <img src={require('../images/tita_color.png')} alt='tita' className='mx-auto img-fluid' />
+                            <img src={require('../images/tita_color.png')} alt='tita' className='mx-auto img-fluid resp_photo' />
                         </div>
                         <div className="col-md">
                            <br/> <br/>
@@ -27,7 +27,7 @@ const AboutMe =()=>{
 
                             <ReactPlayer
                             url='https://www.youtube.com/watch?v=WjcylW50-q4'
-                            className='react-player'
+                            className='react-player video-player'
                             playing
                             
                             width='25rem'
@@ -38,23 +38,23 @@ const AboutMe =()=>{
                     <div className='About-Skills container'>
                         <div className='row'>
                             <div className='col About-container-icon'>
-                                <h2 className='About-info'>Soft Skills</h2>
-                                <h5 className='About-info'>Teamwork</h5>
-                                <h5 className='About-info'>Responsability</h5>
-                                <h5 className='About-info'>Self-learning</h5>
-                                <h5 className='About-info'>Honesty</h5>
-                                <h5 className='About-info'>Empathy</h5>
-                                <h5 className='About-info'>Leadership</h5>
-                                <h5 className='About-info'>Dynamism</h5>
+                                <h2 className='About-info soft'>Soft Skills</h2>
+                                <h5 className='About-info soft'>Teamwork</h5>
+                                <h5 className='About-info soft'>Responsability</h5>
+                                <h5 className='About-info soft'>Self-learning</h5>
+                                <h5 className='About-info soft'>Honesty</h5>
+                                <h5 className='About-info soft'>Empathy</h5>
+                                <h5 className='About-info soft'>Leadership</h5>
+                                <h5 className='About-info soft'>Dynamism</h5>
                             </div>
-                            <div className='col About-container-icon'>
+                            <div className='col About-container-icon About-soft'>
                                 <h2 className='About-info tech'>Technical Skills</h2>
                                 <span className='devicon-javascript-plain colored About-iconskill'></span>
                                 <span className='icon-html5 About-iconskill'></span> 
                                 <span className='icon-css31 About-iconskill'></span>
                                 <span className='icon-bootstrap About-iconskill'></span> 
                                 <span className='devicon-react-original-wordmark colored About-iconskill'></span>
-                                <span className='devicon-photoshop-line colored About-iconskill'></span> <br/> <br/> <br/> <br/>
+                                <span className='devicon-photoshop-line colored About-iconskill'></span> <br/> <br/> <br/>
                                <span className='icon-figma About-iconskill icon-move'></span>
                                 <span className='icon-react About-iconskill'></span>
                                 <span className='devicon-nodejs-plain colored About-iconskill'></span> 
