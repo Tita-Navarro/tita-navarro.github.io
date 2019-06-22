8fa5bdd947374bac5a77b9bc77db17a56f7e6c6b
Martha Navarro <tita1089@gmail.com>
2019-06-21 21:55:53 -0500

    Updates

---
+++ precache-manifest.5bc18b0d9de5c6ed262815524056e5e2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1622779f3f2dd4fb90749d3a617c1a90",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "93f4e4ffe54f3fd7c11a",
+    "url": "/portafolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3e162dd933ad55431366",
+    "url": "/portafolio/static/css/main.a2860050.chunk.css"
+  },
+  {
+    "revision": "93f4e4ffe54f3fd7c11a",
+    "url": "/portafolio/static/js/2.95af3f73.chunk.js"
+  },
+  {
+    "revision": "3e162dd933ad55431366",
+    "url": "/portafolio/static/js/main.acc772fc.chunk.js"
+  },
+  {
+    "revision": "9fd29bcc1d4ba73bed23",
+    "url": "/portafolio/static/js/runtime~main.41300385.js"
+  },
+  {
+    "revision": "f0b910595549fcbb28c6f80947033d1f",
+    "url": "/portafolio/static/media/Castelnav.f0b91059.png"
+  },
+  {
+    "revision": "c4471029d9ca6e8872c28e76872b054f",
+    "url": "/portafolio/static/media/contactme_mickey.c4471029.png"
+  },
+  {
+    "revision": "1fd241d25e4c767a27078e99fc218f45",
+    "url": "/portafolio/static/media/hobbies.1fd241d2.jpg"
+  },
+  {
+    "revision": "3ede531dabe0544a57c32839fba3a417",
+    "url": "/portafolio/static/media/icomoon.3ede531d.ttf"
+  },
+  {
+    "revision": "57c344468f9eeb67572dde0c4932bafa",
+    "url": "/portafolio/static/media/icomoon.57c34446.eot"
+  },
+  {
+    "revision": "83aa3e515f1b50d390a1336923ca469a",
+    "url": "/portafolio/static/media/icomoon.83aa3e51.svg"
+  },
+  {
+    "revision": "cad8b42707479726ef09ab37d1813152",
+    "url": "/portafolio/static/media/icomoon.cad8b427.woff"
+  },
+  {
+    "revision": "d11810c448532e1ef7c70d1eed05ad60",
+    "url": "/portafolio/static/media/mickey-background.d11810c4.jpg"
+  },
+  {
+    "revision": "0c5e27018d5deea91fb0475c7f377abd",
+    "url": "/portafolio/static/media/mickey-background3.0c5e2701.jpg"
+  },
+  {
+    "revision": "6abcbfb38462ba00c58192db52e07e0f",
+    "url": "/portafolio/static/media/mickey-background4.6abcbfb3.jpg"
+  },
+  {
+    "revision": "c2bad8ff23abe1fc324a3e162c79be9a",
+    "url": "/portafolio/static/media/mickey-background5.c2bad8ff.jpg"
+  },
+  {
+    "revision": "6741800f9193e10bae37d778b23ac286",
+    "url": "/portafolio/static/media/mickey_bgipad.6741800f.jpg"
+  },
+  {
+    "revision": "8bfcfbc24f30d04e846c1db20625d264",
+    "url": "/portafolio/static/media/tita_color.8bfcfbc2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f62d6503bbe4e967b490f495d57c38d.js"
+  "/portafolio/precache-manifest.5bc18b0d9de5c6ed262815524056e5e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portafolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
